6abf9c817e825d0c53c12bca1df42f0eb7c292ff
Ivan Onishchenko <Lightsout07@mail.ru>
2022-11-26 13:24:07 +0300

    convertTheTemperature

---
+++ LeetCode_75/level_1/easy/isomorphicStrings/index.js
@@ -0,0 +1,10 @@
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false;
+  const sArr = s.split("");
+  const tArr = t.split("");
+  for (let i = 0; i < sArr.length; i += 1) {
+    let temp = tArr[i];
+  }
+};
+
+console.log(isIsomorphic("egg", "add"));
